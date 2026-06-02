3e0441e4ef084dd6bfaa9d23595f19694cd4c073
Patelabhishek01 <abhishekpatidarexam@gmail.com>
2026-06-02 18:25:58 +0530

    Moved MongoDB config to env

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,10 +2,19 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://abhishekpatidarexam_db_user:a1b2h1i2@ac-bkb3vej-shard-00-00.4pc8ulz.mongodb.net:27017,ac-bkb3vej-shard-00-01.4pc8ulz.mongodb.net:27017,ac-bkb3vej-shard-00-02.4pc8ulz.mongodb.net:27017/erpdb?ssl=true&replicaSet=atlas-be5ckq-shard-0&authSource=admin&retryWrites=true&w=majority");
-    console.log("DB connected");
+    await mongoose.connect(
+      process.env.MONGO_URI
+    );
+
+    console.log("DB Connected");
   } catch (error) {
-    console.log(error);
+    console.error(
+      "Database Connection Error:",
+      error
+    );
+
+    process.exit(1);
   }
 };
+
 module.exports = connectDB;
